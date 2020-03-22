f912f19b4490de2d955ebbb51396edc57731b541
Kritul Rathod <kritulrathod@gmail.com>
2020-03-22 23:50:48 +0530

    Initial Checkin

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetMemoryUtilization
+{
+  public partial class Form1 : Form
+  {
+    string TextBoxContent;
+
+    public Form1()
+    {
+      InitializeComponent();
+      MemoryProfiler.LogMemoryFootPrint(0);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Form form = new Form();
+      for (int i = 0; i <= 100000; i++)
+      {
+        ExportForm(form, i);
+      }
+    }
+
+    private void ExportForm(Form form, int id)
+    {
+      //Fetch record by id and populate the form object
+
+      TextBoxContent += new String('*', id);
+      RichTextBox textbox = new RichTextBox() { Text = TextBoxContent };
+      form.Controls.Add(textbox);
+
+      //Export using the Third Party Export Component
+
+      MemoryProfiler.LogMemoryFootPrint(id);
+    }
+  }
+}
---
+++ MemoryProfiler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetMemoryUtilization
+{
+  public static class MemoryProfiler
+  {
+    static Process currentProcess;
+    static StringBuilder sb;
+
+    public static void LogHeader()
+    {
+      Debug.WriteLine($@"DumpUsedMemory{'\t'}PagedMemorySize64{'\t'}PeakPagedMemorySize64{'\t'}PrivateMemorySize64{'\t'}VirtualMemorySize64");
+    }
+
+    public static void LogMemoryFootPrint(int arg)
+    {
+      sb = new StringBuilder();
+
+      currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+
+      //x86
+      //sb.Append($@",NonpagedSystemMemorySize {currentProcess.NonpagedSystemMemorySize}");
+      //sb.Append($@",PagedMemorySize {currentProcess.PagedMemorySize}");
+      //sb.Append($@",PagedSystemMemorySize {currentProcess.PagedSystemMemorySize}");
+      //sb.Append($@",PeakPagedMemorySize {currentProcess.PeakPagedMemorySize}");
+      //sb.Append($@",PeakVirtualMemorySize {currentProcess.PeakVirtualMemorySize}");
+      //sb.Append($@",PrivateMemorySize {currentProcess.PrivateMemorySize}");
+      //sb.Append($@",VirtualMemorySize {currentProcess.VirtualMemorySize}");
+
+      //x64
+      sb.Append($@"{arg.ToString("00000")}");
+      sb.Append($@"{'\t'}{currentProcess.PagedMemorySize64}");
+      sb.Append($@"{'\t'}{currentProcess.PeakPagedMemorySize64}");
+      sb.Append($@"{'\t'}{currentProcess.PrivateMemorySize64}");
+      sb.Append($@"{'\t'}{currentProcess.VirtualMemorySize64}");
+
+
+      Debug.WriteLine(sb.ToString());
+      sb = null;
+    }
+
+  }
+}
