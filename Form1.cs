10f376c21abb0ceb31d9511d5203f63e377cdf6a
Kritul Rathod <kritulrathod@gmail.com>
2020-03-23 02:33:16 +0530

    added execution duration calculations

--- Form1.cs
+++ Form1.cs
@@ -23,14 +23,23 @@ namespace DotNetMemoryUtilization
     private void button1_Click(object sender, EventArgs e)
     {
       Form form = new Form();
+      var watch = new System.Diagnostics.Stopwatch();
+
+      watch.Start();
+
       for (int i = 0; i <= 100000; i++)
       {
         ExportForm(form, i);
+        ExportForm(i);
       }
+
+      watch.Stop();
+      Debug.WriteLine($@"TotalSeconds: {watch.Elapsed.TotalSeconds}");
     }
 
     private void ExportForm(Form form, int id)
     {
+      Form form = new Form();
       //Fetch record by id and populate the form object
 
       TextBoxContent += new String('*', id);
